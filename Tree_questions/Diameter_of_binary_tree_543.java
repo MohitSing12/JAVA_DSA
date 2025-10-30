7b3182499af7a7d8ef75b2bf5f7b87825b3ceaa7
MohtSing12 <mohitdelhi0987@gmail.com>
2025-10-31 00:24:44 +0530

    Tree_ques

---
+++ Tree_questions/Diameter_of_binary_tree_543.java
@@ -0,0 +1,38 @@
+package Tree_questions;
+
+public class Diameter_of_binary_tree_543 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Solution {
+        public int diameterOfBinaryTree(TreeNode root) {
+int ld=diameterOfBinaryTree(root.left);
+int rd=diameterOfBinaryTree(root.right);
+int sd=ht(root.left)+ht(root.right)+2;
+return Math.max(rd,Math.max(ld,sd));
+        }
+        private int ht(TreeNode root){
+            if(root==null){
+                return -1;
+            }
+            int lh=ht(root.left);
+            int rh=ht(root.right);
+            return Math.max(lh,rh)+1;
+        }
+    }
+}
