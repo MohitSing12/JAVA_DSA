888eb7a2bae25630fbe8fc2c072cc3da0464d939
MohtSing12 <mohitdelhi0987@gmail.com>
2025-11-04 16:59:39 +0530

    BST_ques

---
+++ BST_Questions/Leetcode_701_Insert_into_BST.java
@@ -0,0 +1,40 @@
+package BST_Questions;
+
+public class Leetcode_701_Insert_into_BST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution{
+        public TreeNode insertIntoBST(TreeNode root,int val){
+           return insert(root,val);
+        }
+        private TreeNode insert(TreeNode root,int val){
+            if(root==null){
+                return new TreeNode(val);
+            }
+            if(root.val<val){
+                root.right=insert(root.right,val);
+            }
+            else{
+                root.left=insert(root.left,val);
+            }
+            return root;
+        }
+    }
+}
