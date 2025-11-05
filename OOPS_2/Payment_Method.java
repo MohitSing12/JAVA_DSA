c8a7b1bc29dc24d11e95a041fafe2b3c0df0317e
MohtSing12 <mohitdelhi0987@gmail.com>
2025-11-05 20:35:17 +0530

    OOPS-2

---
+++ OOPS_2/Payment_Method.java
@@ -0,0 +1,19 @@
+package OOPS_2;
+
+public abstract class Payment_Method {
+    public abstract void pay();
+
+    public void fun(){
+
+    }
+
+    public abstract void viewBal();
+
+    public void checkScore(){
+
+    }
+
+    public void Creditamount(){
+
+    }
+}
---
+++ OOPS_2/Upi_Payment.java
@@ -0,0 +1,13 @@
+package OOPS_2;
+
+public class Upi_Payment extends Payment_Method {
+    @Override
+    public void pay(){
+
+    }
+
+    @Override
+    public void viewBal() {
+
+    }
+}
