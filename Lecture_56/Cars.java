588a98cd4e57435de5462222ab2a402d2139f676
MohtSing12 <mohitdelhi0987@gmail.com>
2025-11-06 23:58:06 +0530

    Generic class

---
+++ Lecture_56/Cars.java
@@ -0,0 +1,13 @@
+package Lecture_56;
+
+public class Cars {
+    int speed;
+    int price;
+    String color;
+
+    public Cars(int price,int speed,String color){
+        this.price=price;
+        this.speed=speed;
+        this.color=color;
+    }
+}
