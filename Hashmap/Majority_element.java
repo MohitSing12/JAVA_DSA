80ec527cc1bc0b622c8264e5f2e000ae66c07215
MohtSing12 <mohitdelhi0987@gmail.com>
2025-11-09 00:56:43 +0530

    Map

--- Hashmap/Majority_element.java
+++ Hashmap/Majority_element.java
@@ -7,7 +7,10 @@ public class Majority_element {
         int []arr={1,1,2,2,1,2,2};
         HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
         for(int i=0;i<arr.length;i++){
+
+            //getOrDefault outputs the value of the key entered otherwise it will return a default value passed by the user!!
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+//            System.out.println(map.getOrDefault(arr[i],0));
         }
         for(int x:map.keySet()){
             if(map.get(x)>arr.length/2){
---
+++ Map_Coding_Blocks/map.java
@@ -0,0 +1,50 @@
+package Map_Coding_Blocks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class map {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put("A",10);
+        map.put("B",20);
+        map.put("C",30);
+        map.put("D",40);
+        map.put("E",50);
+        map.put("A",100);
+//        map.put(null,200);
+        System.out.println(map);
+//        System.out.println(map.get("B"));
+//        System.out.println(map.containsKey("A"));
+//        map.remove("C");
+//        System.out.println(map);
+
+
+
+ //TreeMap maintains the sorted order of the map
+ //We can't insert null as a key in treemap because null can't be compared
+        Map<String,Integer> map1=new TreeMap<>();
+        map1.put("A",10);
+        map1.put("B",20);
+        map1.put("C",30);
+        map1.put("D",40);
+        map1.put("E",50);
+        map1.put("A",100);
+//        map1.put(null,200);
+        System.out.println(map1);
+
+ //LinkedHashMap is implemented using Doubly Linked List
+//LinkedHashMap maintains the order of insertion(The manner in which the user enters the key-value)
+        Map<String,Integer> map2=new LinkedHashMap<>();
+        map2.put("A",10);
+        map2.put("B",20);
+        map2.put("C",30);
+        map2.put("D",40);
+        map2.put("E",50);
+        map2.put("A",100);
+//        map2.put(null,200);
+        System.out.println(map2);
+    }
+}
